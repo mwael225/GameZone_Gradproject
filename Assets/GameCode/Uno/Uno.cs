407f66ac4a3f1faae15a06329d6b3789339b82b6
Elshafay1 <104459314+Elshafay1@users.noreply.github.com>
2025-06-20 14:20:51 +0300

    Add files via upload

--- Assets/GameCode/Uno/Uno.cs
+++ Assets/GameCode/Uno/Uno.cs
@@ -7,7 +7,7 @@ using System.Collections;
 using UnityEngine.Playables;
 public class Uno: CardGame
 {
-    string prefabpath = "Prefabs/Card_Deck-Matchup";
+    string prefabpath = "Prefabs/#Card_Deck";
     public bool Playable,reverse,skip = false;
     
     public Uno():base("Uno",4)
@@ -65,54 +65,172 @@ public class Uno: CardGame
         base.MovetoPostion();
         centralPile.First.Value.transform.localPosition = centralpileLocalpos[0];
         centralPile.First.Value.transform.Rotate(0, 180, 0);
-    }
-    public override void throwCard(int player, int cardIndex, string place = "last")
+    }    public override void throwCard(int player, int cardIndex, string place = "last")
     {
-        if(hands[player][navigatedCardindex].name.Split("_")[1] == centralPile.Last.Value.name.Split("_")[1]||hands[player][navigatedCardindex].name.Split("_")[2] == centralPile.Last.Value.name.Split("_")[2])
+        // Get the card name parts for both player's card and top card
+        string[] playerCardParts = hands[player][navigatedCardindex].name.Split("_");
+        string[] topCardParts = centralPile.Last.Value.name.Split("_");
+        
+        // Check if the suit (Hearts, Clubs, etc.) or value (Ace, 2-10, Jack, etc.) matches
+        if(playerCardParts[1] == topCardParts[1] || playerCardParts[2] == topCardParts[2])
         {
-        Playable=true;
-        base.throwCard(player, cardIndex, place);
+            Playable = true;
+            
+            // Store the card value before playing it
+            string cardValue = playerCardParts[2];
+            
+            // Play the card (adds to central pile and removes from hand)
+            base.throwCard(player, cardIndex, place);
+            
+            // Apply special effects based on card value
+            if (cardValue == "Ace")
+            {
+                // Reverse direction - toggle the reverse flag
+                reverse = !reverse;
+                Debug.Log("Direction reversed!");
+            }
+            else if (cardValue == "Queen")
+            {
+                // Skip next player
+                skip = true;
+                Debug.Log("Next player will be skipped!");
+            }
+            else if (cardValue == "Jack")
+            {
+                // Next player draws 2 cards
+                plusTwo(player);
+                Debug.Log("Next player must draw 2 cards!");
+            }
+            else if (cardValue == "King")
+            {
+                // Next player draws 4 cards
+                plus4(player);
+                Debug.Log("Next player must draw 4 cards!");
+            }
         }
         else
         {
-            Debug.Log("that card is not playable");
+            Debug.Log("That card is not playable");
         }
-    }
-    public override GameObject PickCard(int player)
+    }    public override GameObject PickCard(int player)
     {
-            if (deck.Count == 0)
+        // If the deck is empty, recycle cards from discard pile
+        if (deck.Count == 0)
+        {
+            // Attempt to recycle
+            if (!RecycleDeck())
             {
-                Debug.LogWarning("Deck is empty!");
+                // If recycling failed, return null to indicate no card could be drawn
+                Debug.LogWarning("Deck is empty and cannot be recycled. No more cards can be drawn.");
+                return null;
             }
+        }
 
-            GameObject card = deck[deck.Count - 1];
-            deck.RemoveAt(deck.Count - 1);
-            hands[player].Add(card);
-            card.transform.localPosition = handspostions[player][hands[player].Count-1];
-            card.transform.localRotation = Quaternion.Euler(playerrotations[player]);
-            return card;
-    }
-    public void plusTwo(int player)
+        GameObject card = deck[deck.Count - 1];
+        deck.RemoveAt(deck.Count - 1);
+        hands[player].Add(card);
+        card.transform.localPosition = handspostions[player][hands[player].Count-1];
+        card.transform.localRotation = Quaternion.Euler(playerrotations[player]);
+        return card;
+    }public void plusTwo(int player)
     {
-        player +=1 % numberOfPlayers;
+        // Get the next player considering the direction
+        int nextPlayer;
+        if (reverse) {
+            nextPlayer = (player - 1 + numberOfPlayers) % numberOfPlayers;
+        } else {
+            nextPlayer = (player + 1) % numberOfPlayers;
+        }
+        
         for (int i = 0; i < 2; i++)
         {
-            PickCard(player);
+            PickCard(nextPlayer);
         }
     }
     public void plus4(int player)
     {
-        player +=1 % numberOfPlayers;
+        // Get the next player considering the direction
+        int nextPlayer;
+        if (reverse) {
+            nextPlayer = (player - 1 + numberOfPlayers) % numberOfPlayers;
+        } else {
+            nextPlayer = (player + 1) % numberOfPlayers;
+        }
+        
         for (int i = 0; i < 4; i++)
         {
-            PickCard(player);
+            PickCard(nextPlayer);
+        }
+    }    // New method to recycle cards from the discard pile and shuffle them
+    // Returns true if recycling succeeded, false otherwise
+    private bool RecycleDeck()
+    {
+        Debug.Log("Attempting to recycle discard pile...");
+        
+        // We need at least 2 cards in the central pile (one to keep as the top card, at least one to recycle)
+        if (centralPile.Count <= 1)
+        {
+            Debug.LogWarning("Not enough cards in discard pile to recycle!");
+            return false;
+        }
+        
+        // Keep track of the top card
+        GameObject topCard = centralPile.Last.Value;
+        
+        // Create a list to hold all cards except the top one
+        List<GameObject> cardsToRecycle = new List<GameObject>();
+        
+        // Remove the top card first
+        centralPile.RemoveLast();
+        
+        // Get all remaining cards from the central pile
+        while (centralPile.Count > 0)
+        {
+            // Take the first card each time (more efficient for LinkedList)
+            GameObject card = centralPile.First.Value;
+            centralPile.RemoveFirst();
+            cardsToRecycle.Add(card);
+        }
+        
+        // At this point centralPile should be empty
+        Debug.Log("Recycling " + cardsToRecycle.Count + " cards from discard pile.");
+        
+        // Add the top card back to the central pile
+        centralPile.AddLast(topCard);
+        
+        // Reset the position tracker for discard pile to initial value
+        centralpileLocalpos[0] = new Vector3(-1, 0, 0);
+        
+        // Reset position of the top card to original table position
+        topCard.transform.localPosition = centralpileLocalpos[0];
+        topCard.transform.localRotation = Quaternion.Euler(discardpileRotation);
+        
+        // Update the position tracker
+        centralpileLocalpos[0] += discard_pilespcaing[0];
+          // Shuffle the cards into the deck
+        shuffledeck(cardsToRecycle);
+        
+        // Make sure cards are stacked properly in the deck (face down)
+        // This will arrange them with proper spacing and rotation using our overridden Assemble method
+        Assemble(deck);
+        
+        Debug.Log("Successfully recycled " + deck.Count + " cards back into the deck.");
+        return true;
+    }
+    protected override void Assemble(List<GameObject> cards)
+    {
+        // Position deck cards properly on the table
+        float stackOffset = 0f;
+        for (int i = 0; i < cards.Count; i++)
+        {
+            // Position cards horizontally on the table with slight offset for the stack effect
+            cards[i].transform.localPosition = new Vector3(0.5f, 0, stackOffset);
+            
+            // Rotate cards to face down position (laying flat on table, back facing up)
+            cards[i].transform.localRotation = Quaternion.Euler(new Vector3(180, 0, 0));
+            
+            // Add a small vertical offset for each card
+            stackOffset += 0.005f;
         }
     }
-
-    
-
-
-    
-
-
 }
--- Assets/GameCode/Uno/UnoGameManager.cs
+++ Assets/GameCode/Uno/UnoGameManager.cs
@@ -1,61 +1,107 @@
+using System;
+using System.Linq;
+using Mono.Cecil;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameSystem
 {
-    public class UnoGameManager:GameManager
+    public class UnoGameManager : GameManager
     {
-        Uno UnoGame; 
+        Uno UnoGame;
+        private bool hasDrawn = false; // Tracks if the current player has drawn a card this turn
+
         public void Start()
         {
             UnoGame = new Uno();
             UnoGame.gamestate = "playing";
-             
         }
+
         public void Update()
         {
-            if(UnoGame.gamestate!="end")
+            if (UnoGame.gamestate != "end")
             {
                 StartCoroutine(UnoGame.navigatedCards(currentTurn));
-                if(Input.GetKeyDown(KeyCode.Return))
+
+                if (Input.GetKeyDown(KeyCode.Return))
                 {
-                    UnoGame.throwCard(currentTurn,UnoGame.navigatedCardindex);
+                    UnoGame.throwCard(currentTurn, UnoGame.navigatedCardindex);
                     Debug.Log(UnoGame.Playable);
-                    if(UnoGame.Playable)
+                    if (UnoGame.Playable)
                     {
+                        // Check if current player has won (no cards left)
+                        if (UnoGame.hands[currentTurn].Count == 0)
+                        {
+                            Debug.Log("Player " + (currentTurn + 1) + " wins the game!");
+                            UnoGame.gamestate = "end";
+                        }
+                        else
+                        {
+                            // If not, proceed to next player
+                            UnoGame.navigatedCardindex = 0;
+                            currentTurn = NextTurn(UnoGame.numberOfPlayers);
+                            hasDrawn = false; // Reset draw status for next player
+                        }
+                        UnoGame.Playable = false;
+                    }
+                }
+                // Allow drawing only once per turn
+                else if (Input.GetKeyDown(KeyCode.Alpha1) && !hasDrawn)
+                {
+                    GameObject drawnCard = UnoGame.PickCard(currentTurn);
+                    if (drawnCard != null)
+                    {
+                        hasDrawn = true; // Mark that player has drawn a card this turn
+                        Debug.Log("Player drew a card. Press Enter to play it if valid, or press 2 to skip your turn.");
+                    }
+                    else
+                    {
+                        Debug.LogWarning("No cards available to draw. Continue with your turn.");
+                    }
+                }
+                // Add option to skip turn after drawing
+                else if (Input.GetKeyDown(KeyCode.Alpha2) && hasDrawn)
+                {
+                    Debug.Log("Player skipped their turn after drawing.");
                     UnoGame.navigatedCardindex = 0;
                     currentTurn = NextTurn(UnoGame.numberOfPlayers);
-                    if(UnoGame.hands[currentTurn].Count==0)
-                    {
-                        UnoGame.gamestate = "end";
-                    }
-                    UnoGame.Playable = false;
-                    }
-                    
-                }
-                else if(Input.GetKeyDown(KeyCode.Alpha1))
-                {
-                    UnoGame.PickCard(currentTurn);
+                    hasDrawn = false; // Reset draw status for the next player
                 }
             }
-           
+
         }
         public override int NextTurn(int noOfPlayers)
         {
+            int nextPlayer = currentTurn;
+
+            // Determine direction based on reverse status
             if (UnoGame.reverse)
             {
-                currentTurn = (currentTurn - 1 + noOfPlayers) % noOfPlayers;
-                return currentTurn;
-            }
-            else if (UnoGame.skip)
-            {
-                currentTurn = (currentTurn + 2) % noOfPlayers;
-                UnoGame.skip = false;
-                return currentTurn;
+                // Move one player in reverse direction
+                nextPlayer = (nextPlayer - 1 + noOfPlayers) % noOfPlayers;
             }
             else
             {
-                return (currentTurn + 1) % noOfPlayers;
+                // Move one player in normal direction
+                nextPlayer = (nextPlayer + 1) % noOfPlayers;
             }
+
+            // Apply skip if needed - moves one more player in current direction
+            if (UnoGame.skip)
+            {
+                if (UnoGame.reverse)
+                {
+                    nextPlayer = (nextPlayer - 1 + noOfPlayers) % noOfPlayers;
+                }
+                else
+                {
+                    nextPlayer = (nextPlayer + 1) % noOfPlayers;
+                }
+                UnoGame.skip = false; // Reset skip flag
+            }
+
+            return nextPlayer;
         }
     }
 }
